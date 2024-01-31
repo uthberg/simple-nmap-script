ebf6a15a5f42d5cf3a4fe04bf5009eded00494b5
uthberg <115906976+uthberg@users.noreply.github.com>
2024-01-30 18:52:39 -0800

    nmap-script.sh

--- nmap-script.sh
+++ nmap-script.sh
@@ -6,7 +6,7 @@ read IP
 #initiates scan on defined ip
 echo "Scanning target(s): $IP "
 #outputs scan to nmap.txt
-sudo nmap -A -T5 $IP -oN ~/Desktop
+sudo nmap -A -T4 $IP -oN ~/nmap-output.txt
 
 
 
